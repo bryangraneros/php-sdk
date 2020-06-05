d522ee6035f5ecdcbbaca05e9472e2ec5b319c56
bryangraneros <65724327+bryangraneros@users.noreply.github.com>
2020-06-05 10:45:04 -0300

    Update index.php

--- index.php
+++ index.php
@@ -130,8 +130,8 @@ $appName = explode('.', $domain)[0];
                     } else {
                         echo '<p><a alt="Login using MercadoLibre oAuth 2.0" class="btn" href="' . $meli->getAuthUrl($redirectURI, Meli::$AUTH_URL[$siteId]) . '">Authenticate</a></p>';
                     }
+                    print_r ($_SESSION['access_token']);
                     ?>
-
                 </div>
                 <div class="col-sm-6 col-md-6">
                     <h3>Get site</h3>
